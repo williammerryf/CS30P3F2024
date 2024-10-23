bd923dcd9a78a694ce2817f5f107d574b5d275e6
27074009 <27074009@L815WD220008.edu.cbe.ab.ca>
2024-10-23 13:53:37 -0600

    1:53PM

--- Chapter11/src/Mastery/PiggyBank.java
+++ Chapter11/src/Mastery/PiggyBank.java
@@ -1,5 +1,6 @@
 package Mastery;
 
+// Import section
 import java.io.Serializable;
 import java.text.NumberFormat;
 import java.lang.Math;
@@ -7,23 +8,28 @@ import java.lang.Math;
 public class PiggyBank implements Serializable 
 {
 
+	// Variables
 	private double bal;
 	
+	// Constructor
 	public PiggyBank()
 	{
 		bal = 0;
 	}
 	
+	// Constructor
 	public PiggyBank(double b)
 	{
 		bal = b;
 	}
 	
+	// Add money to account in terms of coins
 	public void Add(int p, int n, int d, int q)
 	{
 		bal += p * 0.01 + n * 0.05 + d * 0.1 + q * 0.25;
 	}
 	
+	// Withdraw money by total amount, not coins
 	public void Rmv(double amt)
 	{
 		amt = Math.round(amt * 100) / 100;
@@ -38,6 +44,7 @@ public class PiggyBank implements Serializable
 		}
 	}
 	
+	// toString method, returns balance of account
 	public String toString()
 	{
 		String accountString;

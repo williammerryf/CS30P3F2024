07f04d7973910bfba7140cd26f1201f5fdd9ff49
27074009 <27074009@L815WD220008.edu.cbe.ab.ca>
2024-11-28 15:18:37 -0700

    318pm

--- Phidgets/src/Challenges/Square.java
+++ Phidgets/src/Challenges/Square.java
@@ -6,15 +6,14 @@ import com.phidget22.*;
 public class Square {
 
 	public static int counter = 0;
+	public static int turnCounter = 0;
 	
 	public static void main(String[] args) throws Exception 
 	{
 		
-		//int counter = 0;
-		
 		//Connect to wireless rover
         Net.addServer("", "192.168.100.1", 5661, "", 0);
-
+        
         //Create
         DCMotor leftMotors = new DCMotor();
         DCMotor rightMotors = new DCMotor();
@@ -29,175 +28,139 @@ public class Square {
         rightMotors.open(5000);
         sonar.open(5000);
         
-        /*
-        //Move forward at full speed
-        leftMotors.setTargetVelocity(.5);
-        rightMotors.setTargetVelocity(.5);
-	
-        //Wait for 2 seconds - Moves at velocity for this amount of time before switching to the next line of code, similar to while loop
-        Thread.sleep(3333);
+        int x = 0;
         
-        //Turn in one direction
-        leftMotors.setTargetVelocity(-1);
-        rightMotors.setTargetVelocity(1);
-
-        //Wait for 2 second
-        Thread.sleep(590); // 90 degree turn roughly
-
-        //Stop motors
-        leftMotors.setTargetVelocity(0);
-        rightMotors.setTargetVelocity(0);
-	
-        //Move forward at full speed
-        leftMotors.setTargetVelocity(.5);
-        rightMotors.setTargetVelocity(.5);
-	
-        //Wait for 2 seconds - Moves at velocity for this amount of time before switching to the next line of code, similar to while loop
-        Thread.sleep(3333);
-        */
         
-       // counter = 33;
-       normDirection(leftMotors, rightMotors, sonar, counter);
-  
+        //overrotates when more than 1 turn ex going to two sides of a square
+        // prolly due to wheel accel
+        // change turn or make it stop
         
-        /*
-        while (counter != 4) 
-        {
+        
+        while(x != 2)
+        {     	
 
-            System.out.println("Distance: " + sonar.getDistance() + " mm");
+        	if (turnCounter % 2 == 0)
+        	{
+        		normDirection(leftMotors, rightMotors, sonar);
+        		turnAround(leftMotors, rightMotors, sonar);
+        	}
+        	else
+        	{
+        		revDirection(leftMotors, rightMotors, sonar);
+        		turnAround(leftMotors, rightMotors, sonar);
+        	}
+        	
+        	oneSecStop(leftMotors, rightMotors);
+       	
+        	x++;
             
-            if (sonar.getDistance() < 200) 
-            {
-                //Object detected! Stop motors
-                leftMotors.setTargetVelocity(-0.5);
-                rightMotors.setTargetVelocity(-0.5);
-            } 
-            else 
-            {
-            	
-            	leftMotors.setTargetVelocity(0.5);
-                rightMotors.setTargetVelocity(0.5);
-            	
-                Thread.sleep(3333);
-                
-                //Turn in one direction
-                leftMotors.setTargetVelocity(-1);
-                rightMotors.setTargetVelocity(1);
-
-                //Wait for 2 second
-                Thread.sleep(590); // 90 degree turn roughly
-                counter++;
-            }
             Thread.sleep(100);
         }
-        */
-        
-        
-        /*
-        while(counter != 4)
-        {
-        	if (sonar.getDistance() < 200) {
-                //Object detected! Stop motors
-                leftMotors.setTargetVelocity(-0.5);
-                rightMotors.setTargetVelocity(-0.5);
-                Thread.sleep(100);
-            } else {
-                //Move forward slowly (25% max speed)
-                leftMotors.setTargetVelocity(0.5);
-                rightMotors.setTargetVelocity(0.5);
-                
-            }
-    	
-            //Wait for 2 seconds - Moves at velocity for this amount of time before switching to the next line of code, similar to while loop
-            Thread.sleep(3333);
-            
-            //Turn in one direction
-            leftMotors.setTargetVelocity(-1);
-            rightMotors.setTargetVelocity(1);
 
-            //Wait for 2 second
-            Thread.sleep(590); // 90 degree turn roughly
-            
-            counter ++;
-            
-            
-            
-        }
-        */
-	
-	}
+	 }
 
 	// make a reverse direction version and keep track of counter so it can back track an even amount in case object is infront of it
 	
-	private static void normDirection(DCMotor lM, DCMotor rM,DistanceSensor s/*, int c instead of counter*/) throws Exception 
+	private static void normDirection(DCMotor lM, DCMotor rM, DistanceSensor s) throws Exception 
 	{
-        while(counter != 33) // fix dist travelled toa  meter its a lill over
+        while(counter != 23) // abt a meter
         {
         	lM.setTargetVelocity(0.5);
             rM.setTargetVelocity(0.5);
             counter++;
-            Thread.sleep(101);
+            Thread.sleep(100);
+            
             if (s.getDistance() < 200)
             {
             	break;
             }
         }
         
-        if(counter == 33)
+        if(counter == 23)
         {
         	//Turn in one direction
-            lM.setTargetVelocity(-1);
+            lM.setTargetVelocity(-0.8);
             rM.setTargetVelocity(1);
 
             Thread.sleep(810); // 90 degree turn roughly
-            
+            /*
             lM.setTargetVelocity(0);
             rM.setTargetVelocity(0);
+            Thread.sleep(1000);
+            */
+            counter = 0;
 
         }
+
+        lM.setTargetVelocity(0);
+        rM.setTargetVelocity(0);
+        Thread.sleep(1000);
         
         return;
 
 	}
 	
-	
-	// didnt test with counter variable here
-	private static int revDirection(DCMotor lM, DCMotor rM,DistanceSensor s, int c) throws Exception 
+	private static void revDirection(DCMotor lM, DCMotor rM, DistanceSensor s) throws Exception 
 	{
-		while(c != 33) // fix dist travelled toa  meter its a lill over
+        while(counter != 23) // abt a meter
         {
-        	lM.setTargetVelocity(-0.5);
-            rM.setTargetVelocity(-0.5);
-        	c++;
-            Thread.sleep(101);
+        	lM.setTargetVelocity(0.5);
+            rM.setTargetVelocity(0.5);
+            counter++;
+            Thread.sleep(100);
+            
             if (s.getDistance() < 200)
             {
             	break;
             }
         }
         
-        if(c == 33)
+        if(counter == 23)
         {
         	//Turn in one direction
             lM.setTargetVelocity(1);
-            rM.setTargetVelocity(-1);
+            rM.setTargetVelocity(-0.8);
 
             Thread.sleep(810); // 90 degree turn roughly
-            
+            /*
             lM.setTargetVelocity(0);
             rM.setTargetVelocity(0);
-            
-            return 0;
+            Thread.sleep(1000);
+            */
+            counter = 0;
+
         }
-        else
-        {
-        	return c;
-        }
+                
+        return;
+
 	}
 	
-	private int getCounter()
+	private static void turnAround(DCMotor lM, DCMotor rM, DistanceSensor s) throws Exception 
 	{
-		return(counter);
+		if (s.getDistance() < 200) 
+        {
+        	counter = 23 - counter; 
+        	
+        	lM.setTargetVelocity(-1);
+            rM.setTargetVelocity(1);
+
+            Thread.sleep(1200);
+        	
+        	turnCounter ++;
+        } 
+		else
+		{
+			return;
+		}
+	}
+	
+	private static void oneSecStop(DCMotor lM, DCMotor rM) throws Exception
+	{
+		lM.setTargetVelocity(0);
+        rM.setTargetVelocity(0);
+
+        Thread.sleep(100);
+		return;
 	}
 	
 }
